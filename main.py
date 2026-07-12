bca5d6fdb809ae51b52459da4f8d7c24c37ac799
yuiiton <hebertruan00@email.com>
2026-07-12 09:51:49 -0300

    feat: adiciona sincronização de eventos com banco de dados

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 from core.database import create_db
+from core.database import get_session
+from sources.ifs import get_events
+from core import repository
 
-create_db()
+def sync_events():
+    events = get_events()
+
+    with get_session() as session:
+        for event in events:
+            if not repository.exists(session, event):
+                repository.save(session, event)
+                print(f"Novo evento: {event.title}")
+            else:
+                print(f"Já existe: {event.title}")
+
+def main():
+    create_db()
+    sync_events()
+
+if __name__ == "__main__":
+    main()
