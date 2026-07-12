db584efa741b0ab9822efeb6f0a9102f03e23f22
yuiiton <hebertruan00@email.com>
2026-07-12 08:41:32 -0300

    feat: adiciona conexão com o banco de dados SQLite

---
+++ core/database.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel, Session, create_engine
+
+DATABASE_URL = "sqlite:///data/monitor.db"
+
+engine = create_engine(DATABASE_URL)
+
+def create_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    return Session(engine)
